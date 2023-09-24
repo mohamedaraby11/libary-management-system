7c3ab6a7c6c956bc77c8c4ffcc3c0171b9613d47
mohamedaraby <mohamed.araby288@gmail.com>
2023-09-24 19:35:11 +0300

    Finish borrowers endpoint

--- app.js
+++ app.js
@@ -8,108 +8,165 @@ app.use(express.json());
 // Define a route for the root URL
 app.get('/', (req, res) => {
     res.send('Welcome to the Library Management System');
-  });
-  
-  // Book Endpoints
-  app.get('/books', async (req, res) => {
+});
+
+// Book Endpoints
+app.get('/books', async (req, res) => {
     try {
-      const { rows } = await pool.query('SELECT * FROM books');
-      res.json(rows);
+        const { rows } = await pool.query('SELECT * FROM books');
+        res.json(rows);
     } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Internal server error' });
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
     }
-  });
+});
 
 
 // Add a book
 app.post('/books', async (req, res) => {
     const { title, author, isbn, quantity, shelf_location } = req.body;
-  
+
     try {
-      const { rows } = await pool.query(
-        'INSERT INTO books (title, author, isbn, quantity, shelf_location) VALUES ($1, $2, $3, $4, $5) RETURNING *',
-        [title, author, isbn, quantity, shelf_location]
-      );
-  
-      res.status(201).json(rows[0]);
+        const { rows } = await pool.query(
+            'INSERT INTO books (title, author, isbn, quantity, shelf_location) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+            [title, author, isbn, quantity, shelf_location]
+        );
+
+        res.status(201).json(rows[0]);
     } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Internal server error' });
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
     }
-  });
-  
-  // Update a book's details
-  app.put('/books/:id', async (req, res) => {
+});
+
+// Update a book's details
+app.put('/books/:id', async (req, res) => {
     const bookId = req.params.id;
     const { title, author, isbn, quantity, shelf_location } = req.body;
-  
+
     try {
-      const { rows } = await pool.query(
-        'UPDATE books SET title=$1, author=$2, isbn=$3, quantity=$4, shelf_location=$5 WHERE id=$6 RETURNING *',
-        [title, author, isbn, quantity, shelf_location, bookId]
-      );
-  
-      if (rows.length === 0) {
-        res.status(404).json({ error: 'Book not found' });
-      } else {
-        res.json(rows[0]);
-      }
+        const { rows } = await pool.query(
+            'UPDATE books SET title=$1, author=$2, isbn=$3, quantity=$4, shelf_location=$5 WHERE id=$6 RETURNING *',
+            [title, author, isbn, quantity, shelf_location, bookId]
+        );
+
+        if (rows.length === 0) {
+            res.status(404).json({ error: 'Book not found' });
+        } else {
+            res.json(rows[0]);
+        }
     } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Internal server error' });
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
     }
-  });
-  
-  // Delete a book
-  app.delete('/books/:id', async (req, res) => {
+});
+
+// Delete a book
+app.delete('/books/:id', async (req, res) => {
     const bookId = req.params.id;
-  
+
     try {
-      const { rows } = await pool.query('DELETE FROM books WHERE id=$1 RETURNING *', [bookId]);
-  
-      if (rows.length === 0) {
-        res.status(404).json({ error: 'Book not found' });
-      } else {
-        res.json({ message: 'Book deleted successfully' });
-      }
+        const { rows } = await pool.query('DELETE FROM books WHERE id=$1 RETURNING *', [bookId]);
+
+        if (rows.length === 0) {
+            res.status(404).json({ error: 'Book not found' });
+        } else {
+            res.json({ message: 'Book deleted successfully' });
+        }
     } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Internal server error' });
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
     }
-  });
-  
-  // Search for a book by title, author, or ISBN
-  app.get('/books/search', async (req, res) => {
+});
+
+// Search for a book by title, author, or ISBN
+app.get('/books/search', async (req, res) => {
     const { title, author, isbn } = req.query;
-  
+
     try {
-      const { rows } = await pool.query(
-        'SELECT * FROM books WHERE title ILIKE $1 OR author ILIKE $2 OR isbn ILIKE $3',
-        [`%${title}%`, `%${author}%`, `%${isbn}%`]
-      );
-  
-      res.json(rows);
+        const { rows } = await pool.query(
+            'SELECT * FROM books WHERE title ILIKE $1 OR author ILIKE $2 OR isbn ILIKE $3',
+            [`%${title}%`, `%${author}%`, `%${isbn}%`]
+        );
+
+        res.json(rows);
     } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Internal server error' });
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
     }
-  });
+});
 
 
 //Borrowers Endpoints 
 
 // List all borrowers
-    app.get('/borrowers', async (req, res) => {
-        try {
-          const { rows } = await pool.query('SELECT * FROM borrowers');
-          res.json(rows);
-        } catch (err) {
-          console.error(err);
-          res.status(500).json({ error: 'Internal server error' });
+app.get('/borrowers', async (req, res) => {
+    try {
+        const { rows } = await pool.query('SELECT * FROM borrowers');
+        res.json(rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+// Register a borrower
+app.post('/borrowers', async (req, res) => {
+    const { name, email, registered_date } = req.body;
+
+    try {
+        const { rows } = await pool.query(
+            'INSERT INTO borrowers (name, email, registered_date) VALUES ($1, $2, $3) RETURNING *',
+            [name, email, registered_date]
+        );
+
+        res.status(201).json(rows[0]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+// Update borrower's details
+app.put('/borrowers/:id', async (req, res) => {
+    const borrowerId = req.params.id;
+    const { name, email, registered_date } = req.body;
+
+    try {
+        const { rows } = await pool.query(
+            'UPDATE borrowers SET name=$1, email=$2, registered_date=$3 WHERE id=$4 RETURNING *',
+            [name, email, registered_date, borrowerId]
+        );
+
+        if (rows.length === 0) {
+            res.status(404).json({ error: 'Borrower not found' });
+        } else {
+            res.json(rows[0]);
         }
-      });
-  
-  app.listen(3000, () => {
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+// Delete a borrower
+app.delete('/borrowers/:id', async (req, res) => {
+    const borrowerId = req.params.id;
+
+    try {
+        const { rows } = await pool.query('DELETE FROM borrowers WHERE id=$1 RETURNING *', [borrowerId]);
+
+        if (rows.length === 0) {
+            res.status(404).json({ error: 'Borrower not found' });
+        } else {
+            res.json({ message: 'Borrower deleted successfully' });
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+
+app.listen(3000, () => {
     console.log('Server is running on port 3000');
-  });
+});

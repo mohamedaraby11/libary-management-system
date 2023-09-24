fa2342465353165abf1456532c1944ae2356d7c6
mohamedaraby <mohamed.araby288@gmail.com>
2023-09-24 18:50:29 +0300

    Add

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ app.get('/', (req, res) => {
     res.send('Welcome to the Library Management System');
   });
   
-  // Define the "/books" route as you have already done
   app.get('/books', async (req, res) => {
     try {
       const { rows } = await pool.query('SELECT * FROM books');
@@ -20,6 +19,86 @@ app.get('/', (req, res) => {
       res.status(500).json({ error: 'Internal server error' });
     }
   });
+
+
+// Add a book
+app.post('/books', async (req, res) => {
+    const { title, author, isbn, quantity, shelf_location } = req.body;
+  
+    try {
+      const { rows } = await pool.query(
+        'INSERT INTO books (title, author, isbn, quantity, shelf_location) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+        [title, author, isbn, quantity, shelf_location]
+      );
+  
+      res.status(201).json(rows[0]);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+  
+  // Update a book's details
+  app.put('/books/:id', async (req, res) => {
+    const bookId = req.params.id;
+    const { title, author, isbn, quantity, shelf_location } = req.body;
+  
+    try {
+      const { rows } = await pool.query(
+        'UPDATE books SET title=$1, author=$2, isbn=$3, quantity=$4, shelf_location=$5 WHERE id=$6 RETURNING *',
+        [title, author, isbn, quantity, shelf_location, bookId]
+      );
+  
+      if (rows.length === 0) {
+        res.status(404).json({ error: 'Book not found' });
+      } else {
+        res.json(rows[0]);
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+  
+  // Delete a book
+  app.delete('/books/:id', async (req, res) => {
+    const bookId = req.params.id;
+  
+    try {
+      const { rows } = await pool.query('DELETE FROM books WHERE id=$1 RETURNING *', [bookId]);
+  
+      if (rows.length === 0) {
+        res.status(404).json({ error: 'Book not found' });
+      } else {
+        res.json({ message: 'Book deleted successfully' });
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+  
+  // Search for a book by title, author, or ISBN
+  app.get('/books/search', async (req, res) => {
+    const { title, author, isbn } = req.query;
+  
+    try {
+      const { rows } = await pool.query(
+        'SELECT * FROM books WHERE title ILIKE $1 OR author ILIKE $2 OR isbn ILIKE $3',
+        [`%${title}%`, `%${author}%`, `%${isbn}%`]
+      );
+  
+      res.json(rows);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+
+
+
+  
+  
   
   app.listen(3000, () => {
     console.log('Server is running on port 3000');
